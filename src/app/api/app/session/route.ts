feb0f848f6af236a19123f8359a433dcc40fec34
aditwr <awahyu148@gmail.com>
2025-03-14 08:38:08 +0700

    fixing bug in app session

--- src/app/api/app/session/route.ts
+++ src/app/api/app/session/route.ts
@@ -3,11 +3,23 @@ import { APIRequestResponse } from "@/types/types";
 import { NextResponse } from "next/server";
 
 export async function GET(): Promise<NextResponse<APIRequestResponse>> {
-    const supabase = await createClient();
-    const { data: { session }, error: errorGetSession } = await supabase.auth.getSession();
-    
-    if (errorGetSession) {
-        return NextResponse.json({ success: false, message: "Internal Server Error" });
-    }
-    return NextResponse.json({ success: true, message: "get session success", data: session });
+  const supabase = await createClient();
+  const {
+    data: { session },
+    error: errorGetSession,
+  } = await supabase.auth.getSession();
+
+  if (errorGetSession) {
+    return NextResponse.json({
+      success: false,
+      status: 500,
+      message: "Internal Server Error",
+    });
+  }
+  return NextResponse.json({
+    success: true,
+    status: 200,
+    message: "get session success",
+    data: session,
+  });
 }
