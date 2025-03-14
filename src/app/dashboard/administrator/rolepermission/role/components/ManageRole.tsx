07ea798c01a340cb5118a290e9460c2b17f40966
aditwr <awahyu148@gmail.com>
2025-03-14 10:05:09 +0700

    update

--- src/app/dashboard/administrator/rolepermission/role/components/ManageRole.tsx
+++ src/app/dashboard/administrator/rolepermission/role/components/ManageRole.tsx
@@ -2,13 +2,20 @@
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getRoles } from "../services/roleService";
 import { LoadingTopLevel } from "@/components/ui/Loading";
-import { Button, Table, Modal } from "antd";
+import { Button, Table, Modal, TableColumnsType } from "antd";
 import { convertRolesToDataSource } from "../services/helpers";
 import React, { useState } from "react";
 import { ExclamationCircleFilled } from "@ant-design/icons";
 import { Role } from "@/types/data";
 
 const { confirm } = Modal;
+
+interface DataType {
+  key: string;
+  no: string;
+  role: string;
+  created_at: Date;
+}
 
 export default function ManageRole() {
   const queryClient = useQueryClient();
@@ -29,57 +36,66 @@ export default function ManageRole() {
   const dataSource = convertRolesToDataSource(roles.data);
 
   const showDeleteConfirm = (role: Role) => {
-  confirm({
-    title: "Yakin ingin menghapus Role?",
-    icon: <ExclamationCircleFilled />,
-    content: (
-      <p>
-        Apakah anda yakin ingin menghapus Role <strong>{role.role}</strong>?
-      </p>
-    ),
-    okText: "Yes",
-    okType: "danger",
-    cancelText: "No",
-    onOk() {
-      console.log("OK");
-    },
-    onCancel() {
-      console.log("Cancel");
-    },
-  });
-};
+    confirm({
+      title: "Yakin ingin menghapus Role?",
+      icon: <ExclamationCircleFilled />,
+      content: (
+        <p>
+          Apakah anda yakin ingin menghapus Role <strong>{role.role}</strong>?
+        </p>
+      ),
+      okText: "Yes",
+      okType: "danger",
+      cancelText: "No",
+      onOk() {
+        console.log("OK");
+      },
+      onCancel() {
+        console.log("Cancel");
+      },
+    });
+  };
 
-const columns = [
-  {
-    title: "No",
-    dataIndex: "no",
-    key: "no",
-  },
-  {
-    title: "Role",
-    dataIndex: "role",
-    key: "role",
-  },
-  {
-    title: "Action",
-    dataIndex: "action",
-    key: "action",
-    render: (role: Role, record: any) => (
-      <Button onClick={() => showDeleteConfirm(role)} danger>
-        Delete
-      </Button>
-    ),
-  },
-];
+  const columns: TableColumnsType<DataType> = [
+    {
+      title: "No",
+      dataIndex: "no",
+      key: "no",
+    },
+    {
+      title: "Role",
+      dataIndex: "role",
+      key: "role",
+      defaultSortOrder: "descend",
+      sorter: (a: DataType, b: DataType) => a.role - b.role,
+    },
+    {
+      title: "Created at",
+      dataIndex: "created_at",
+      key: "created_at",
+    },
+    {
+      title: "Action",
+      dataIndex: "action",
+      key: "action",
+      render: (role: Role, record: any) => (
+        <Button onClick={() => showDeleteConfirm(role)} danger>
+          Delete
+        </Button>
+      ),
+    },
+  ];
 
   return (
     <div className="">
       <div className="w-1/2">
-        <Table
+        <Table<DataType>
           dataSource={dataSource}
           columns={columns}
+          onChange={(pagination, filters, sorter) => {
+            console.log(sorter);
+          }}
           size="small"
-          pagination={false}
         />
       </div>
     </div>
--- src/app/dashboard/administrator/rolepermission/role/services/helpers.ts
+++ src/app/dashboard/administrator/rolepermission/role/services/helpers.ts
@@ -1,19 +1,22 @@
 interface Role {
   id: number;
   role: string;
+  created_at: Date;
 }
 
-interface DataSourceItem {
+interface DataType {
   key: string;
   no: string;
   role: string;
+  created_at: Date;
 }
 
-export function convertRolesToDataSource(roles: Role[]): DataSourceItem[] {
+export function convertRolesToDataSource(roles: Role[]): DataType[] {
   return roles.map((item, index) => ({
     key: String(index + 1),
     no: String(index + 1),
     role: item.role,
+    created_at: item.created_at,
     action: item,
   }));
 }
